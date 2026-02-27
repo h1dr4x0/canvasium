10a3f8e3586b6d9c2e742d052fc4444bec27246e
h1dr4x0 <116759699+h1dr4x0@users.noreply.github.com>
2026-02-27 11:14:17 +0300

    Delete replace_variables.js

--- src/utils/replace_variables.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.replaceVars = replaceVars;
-function replaceVars(str, data) {
-    if (data === void 0) { data = {}; }
-    return str.replace(/\{(\w+)\}/g, function (_, key) { var _a; return (_a = data[key]) !== null && _a !== void 0 ? _a : "{".concat(key, "}"); });
-}
